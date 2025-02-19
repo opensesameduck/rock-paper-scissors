3050ca3877e670b9c63781771b340a39fd99f70c
Lucy Liu <194132503+opensesameduck@users.noreply.github.com>
2025-02-18 20:14:44 -0500

    Write the logic to get computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello Pumpkin!")
+
+function getComputerChoice() {
+    let randInt = Math.floor(Math.random() * 3);
+    
+    if (randInt == 0) {
+        return "rock";
+    } else if (randInt == 1) {
+        return "paper";
+    } else if (randInt == 2) {
+        return "scissor";
+    }
+    
+}
+
+console.log(getComputerChoice())
